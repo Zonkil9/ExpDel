7dd05424e956bd4f3b94de935399f2f0444b9b0f
Zonkil9 <grzegorz.skrzynski@us.edu.pl>
2025-06-19 14:45:56 +0200

    Listing files by date

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::fs;
+use std::io;
+use std::path;
+use std::time;
+use chrono::{DateTime, Local};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter the path");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    
+    list_by_date(input);
+}
+
+fn list_by_date(input: String) -> io::Result<()> {
+    let path = path::PathBuf::from(input.trim());
+    println!("Opening {}", path.display());
+
+    let mut entries: Vec<path::PathBuf> = fs::read_dir(&path)?
+        .filter_map(|res| res.ok().map(|e| e.path()))
+        .filter(|p| fs::metadata(p).is_ok())
+        .collect();
+
+    entries.sort_by_key(|k| {
+        fs::metadata(k)
+            .and_then(|m| m.modified())
+            .ok()
+    });
+
+    for entry in entries {
+        let metadata = fs::metadata(&entry)?;
+        let modified: time::SystemTime = metadata.modified()?;
+        let datetime: DateTime<Local> = modified.into();
+        println!("Name: {} | Date: {}", entry.display(), datetime.format("%Y-%m-%d %H:%M:%S"));
+    }
+
+    Ok(())
 }
