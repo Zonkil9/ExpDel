8d4c70d7c9888d8278cd445b1c0a323d4ca9e6b8
Zonkil9 <grzegorz.skrzynski@us.edu.pl>
2025-06-22 16:56:35 +0200

    Added mtime, ctime, atime

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,40 @@ use std::path;
 use std::time;
 use chrono::{DateTime, Local};
 
+#[derive(Debug)]
+enum SortType {
+    MTime,
+    CTime,
+    ATime,
+}
+
 fn main() {
     println!("Please enter the path");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
+    println!("Sorting with: mtime / ctime / atime");
+    let mut sort_type = String::new();
+    io::stdin().read_line(&mut sort_type).expect("Failed to read line");
+
+    let sort_type = match sort_type.trim() {
+        "mtime" => SortType::MTime,
+        "ctime" => SortType::CTime,
+        "atime" => SortType::ATime,
+        _ => {
+            eprintln!("Invalid sort type. Defaulting to mtime.");
+            SortType::MTime
+        }
+    };
+
     let path = path::Path::new(input.trim());
 
-    list_by_date(path)
+    list_by_date(path, sort_type)
         .unwrap_or_else(|err| eprintln!("Error: {}", err));
 }
 
-fn list_by_date(path: &path::Path) -> io::Result<()> {
-    println!("Opening {}", path.display());
+fn list_by_date(path: &path::Path, sort_type: SortType) -> io::Result<()> {
+    println!("Opening {} and sorting by {:?}", path.display(), sort_type);
 
     let mut entries: Vec<path::PathBuf> = fs::read_dir(&path)?
         .filter_map(|res| res.ok().map(|e| e.path()))
@@ -25,16 +46,24 @@ fn list_by_date(path: &path::Path) -> io::Result<()> {
 
     entries.sort_by_key(|k| {
         fs::metadata(k)
-            .and_then(|m| m.modified())
-            .ok()
+            .map(|m| get_time_type(&m, &sort_type))
+            .unwrap_or(time::UNIX_EPOCH)
     });
 
     for entry in entries {
         let metadata = fs::metadata(&entry)?;
-        let modified: time::SystemTime = metadata.modified()?;
-        let datetime: DateTime<Local> = modified.into();
+        let time = get_time_type(&metadata, &sort_type);
+        let datetime: DateTime<Local> = time.into();
         println!("Name: {} | Date: {}", entry.display(), datetime.format("%Y-%m-%d %H:%M:%S"));
     }
 
     Ok(())
+}
+
+fn get_time_type(meta: &fs::Metadata, sort_type: &SortType) -> time::SystemTime {
+    match sort_type {
+        SortType::MTime => meta.modified().unwrap_or(time::UNIX_EPOCH),
+        SortType::ATime => meta.accessed().unwrap_or(time::UNIX_EPOCH),
+        SortType::CTime => meta.created().unwrap_or(time::UNIX_EPOCH),
+    }
 }
